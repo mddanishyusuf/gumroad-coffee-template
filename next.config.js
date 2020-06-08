fa99356a4b4775d66eff1996b4f45e669d6f08a3
mohd danish <mddanishyusuf@gmail.com>
2020-06-08 17:00:10 +0530

    adding config file

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+const withSass = require('@zeit/next-sass');
+module.exports = withSass({
+    /* config options here */
+    // env: {
+    //     GUMROAD_ACCESS_TOKEN: '83cacc55d45238c8e7fac7aaa66c34a613f5ce6ae51a321e51f37ed903763f88',
+    // },
+});
