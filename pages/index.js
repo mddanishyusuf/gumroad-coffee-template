d1963e54a3a6a4099b176691068506ee820f81f6
Mohd Danish <mddanishyusuf@gmail.com>
2022-10-09 17:32:00 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
             <HeadMeta />
             <div className="github-link">
                 <span>
-                    Coffee page with Gumroad payment
+                    Coffee page with Gumroad
                 </span>
                 <a href="https://github.com/mddanishyusuf/gumroad-coffee-template" target="_blank"><AiOutlineCode /><span>Get the Code</span></a>
                 <a href="https://github.com/mddanishyusuf/gumroad-coffee-template/blob/master/README.md" target="_blank"><FiGithub /><span>Use this template</span></a>
